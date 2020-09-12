7455e08e79309cbb321f1d7304fef4e507417c26
Jordan Tucker <jordanbtucker@gmail.com>
2020-09-12 17:04:20 -0500

    test: test all fixtures

--- test/yaz0/decompression.js
+++ test/yaz0/decompression.js
@@ -1,23 +1,23 @@
 const t = require('tap')
 const {deepStrictEqual, strictEqual} = require('assert')
 const {createReadStream} = require('fs')
+const {join} = require('path')
+const {readdir} = require('fs').promises
 
 const {decompress} = require('../../').yaz0
 
 t.test('decompress', async t => {
-  t.setTimeout(0)
-  await assertStreamsAreEqual(
-    createReadStream('test/yaz0/fixtures/compressed/0-0.shknm2').pipe(
-      decompress(),
-    ),
-    createReadStream('test/yaz0/fixtures/decompressed/0-0.shknm2'),
-  )
-  await assertStreamsAreEqual(
-    createReadStream(
-      'test/yaz0/fixtures/compressed/ActorInfo.product.sbyml',
-    ).pipe(decompress()),
-    createReadStream('test/yaz0/fixtures/decompressed/ActorInfo.product.sbyml'),
-  )
+  const fixtureNames = await readdir('test/yaz0/fixtures/compressed')
+  for (const fixtureName of fixtureNames) {
+    await t.test(fixtureName, async () => {
+      await assertStreamsAreEqual(
+        createReadStream(
+          join('test/yaz0/fixtures/compressed/', fixtureName),
+        ).pipe(decompress()),
+        createReadStream(join('test/yaz0/fixtures/decompressed/', fixtureName)),
+      )
+    })
+  }
 })
 
 /**
